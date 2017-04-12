2b8f5699cd94650141d484e3a4ca7a0ec5e08e57
rageworx <rageworx@gmail.com>
2017-04-12 17:54:27 +0900

    Updating UI improvements

--- res/resource.h
+++ res/resource.h
@@ -1,3 +1,3 @@
-#define APP_VERSION								0,1,2,12
-#define APP_VERSION_STR							"0.1.2.12"
+#define APP_VERSION								0,1,2,13
+#define APP_VERSION_STR							"0.1.2.13"
 #define IDC_ICON_A								101
